e263128a4f6872b5adaf7107ba55e3e74f7226f7
alainabbas <alain.abbas@libertech.fr>
2024-06-10 15:29:19 +0000

    Deploying to gh-pages from @ Libertech-FR/sesame-doc@7fee1fa53b9c9ef3f7709cd6906afb72dd21d5aa 🚀

---
+++ assets/installation-taiga.html-0gwZYfpC.js
@@ -0,0 +1,15 @@
+import{_ as a,c as n,o as s,a as i}from"./app-B7D3Nb6M.js";const e={},l=i(`<h1 id="installation-import-taiga-ecoles-archi" tabindex="-1"><a class="header-anchor" href="#installation-import-taiga-ecoles-archi"><span>Installation Import Taiga (Ecoles archi)</span></a></h1><p>Après avoir installé sesame lancez le script d&#39;installation taiga</p><div class="language-text line-numbers-mode" data-highlighter="prismjs" data-ext="text" data-title="text"><pre class="language-text"><code><span class="line">./install/install-taiga.sh</span>
+<span class="line"></span>
+<span class="line">Parametres de connexion à TAIGA</span>
+<span class="line">-------------------------------</span>
+<span class="line">URL TAIGA (https://taiga.archi.fr) :https://taiga.archi.fr</span>
+<span class="line">PORT (443) : 443</span>
+<span class="line">UTILISATEUR TAIGA :monlogintaiga</span>
+<span class="line">MOT DE PASSE : monpasswdtaiga</span>
+<span class="line">MOT DE PAS ENSA : monmdpensataiga</span>
+<span class="line">Nom de domaine des emails : mondomaine.fr</span>
+<span class="line">Numero d&#39;etablissement SUPANN : 123456U</span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>Renseignez vos identifiants et les informations pour la connexion taiga</p><div class="language-text line-numbers-mode" data-highlighter="prismjs" data-ext="text" data-title="text"><pre class="language-text"><code><span class="line">------------------------------</span>
+<span class="line">L&#39;installation est terminée</span>
+<span class="line">Vous pouvez vous connecter à l interface via http://10.22.32.66:3000</span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p><em>Sesame est installé</em></p>`,6),t=[l];function r(c,d){return s(),n("div",null,t)}const p=a(e,[["render",r],["__file","installation-taiga.html.vue"]]),m=JSON.parse('{"path":"/installation/installation-taiga.html","title":"Installation import Taiga","lang":"en-US","frontmatter":{"lang":"en-US","title":"Installation import Taiga","description":"Installation import Taiga"},"headers":[],"git":{"updatedTime":1718033316000,"contributors":[{"name":"Alain Abbas","email":"alain.abbas@libertech.fr","commits":1}]},"filePathRelative":"installation/installation-taiga.md"}');export{p as comp,m as data};
