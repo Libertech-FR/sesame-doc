a943e7bb10b2c4c59d0e4c83fb6da5760aeb257b
tacxou <12997062+tacxou@users.noreply.github.com>
2025-12-02 08:22:19 +0000

    Deploying to gh-pages from @ Libertech-FR/sesame-doc@941c2d629f01be143916645d53f02f6f6cb2d7c2 🚀

--- assets/404.html-BNdA4pg8.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,c as e,o,b as a}from"./app-CNo3EHYg.js";const n={},c=a("p",null,"404 Not Found",-1),l=[c];function s(r,_){return o(),e("div",null,l)}const d=t(n,[["render",s],["__file","404.html.vue"]]),f=JSON.parse('{"path":"/404.html","title":"","lang":"fr-FR","frontmatter":{"layout":"NotFound"},"headers":[],"git":{},"filePathRelative":null}');export{d as comp,f as data};
